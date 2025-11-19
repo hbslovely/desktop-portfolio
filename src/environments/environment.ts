faf02e5723395d52746ebbe9af33971a651c2862
Phat Hong <phat@backbase.com>
2025-11-19 16:21:57 +0700

    Add expense app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: false,
   newsApiToken: '3PCVToRTlD2xuA6UTdEJqDabqrPUUGPbqtICKzug',
   newsApiUrl: 'https://api.thenewsapi.com/v1/news/top',
-  weatherApiKey: process.env['WEATHER_API_KEY'],
+  weatherApiKey: import.meta.env['WEATHER_API_KEY'],
   weatherApiUrl: 'https://www.meteosource.com/api/v1/free',
-  googleSheetsApiKey: process.env['GOOGLE_SHEETS_API_KEY'],
-  googleClientId: process.env['NG_APP_GOOGLE_CLIENT_ID'],
-  googleClientSecret: process.env['NG_APP_GOOGLE_CLIENT_SECRET'],
-  googleAppsScriptUrl: process.env['GOOGLE_APPS_SCRIPT_URL'],
+  googleSheetsApiKey: import.meta.env['GOOGLE_SHEETS_API_KEY'],
+  googleClientId: import.meta.env['NG_APP_GOOGLE_CLIENT_ID'],
+  googleClientSecret: import.meta.env['NG_APP_GOOGLE_CLIENT_SECRET'],
+  googleAppsScriptUrl: import.meta.env['GOOGLE_APPS_SCRIPT_URL'],
 };
 
