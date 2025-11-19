9fa203d7fa7710592a73248b343368ec51d2a73e
Phat Hong <phat@backbase.com>
2025-11-19 17:05:08 +0700

    Add expense app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  newsApiToken: import.meta.env['NEWS_API_TOKEN'] || '',
+  newsApiToken: import.meta.env['NG_APP_NEWS_API_TOKEN'] || '',
   newsApiUrl: 'https://api.thenewsapi.com/v1/news/headlines',
-  weatherApiKey: import.meta.env['WEATHER_API_KEY'] ,
+  weatherApiKey: import.meta.env['NG_APP_WEATHER_API_KEY'] ,
   weatherApiUrl: 'https://www.meteosource.com/api/v1/free',
-  googleSheetsApiKey: import.meta.env['GOOGLE_SHEETS_API_KEY'],
+  googleSheetsApiKey: import.meta.env['NG_APP_GOOGLE_SHEETS_API_KEY'],
   googleClientId: import.meta.env['NG_APP_GOOGLE_CLIENT_ID'],
   googleClientSecret: import.meta.env['NG_APP_GOOGLE_CLIENT_SECRET'],
-  googleAppsScriptUrl: import.meta.env['GOOGLE_APPS_SCRIPT_URL'],
+  googleAppsScriptUrl: import.meta.env['NG_APP_GOOGLE_APPS_SCRIPT_URL'],
 };
 
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,11 +1,10 @@
 interface ImportMetaEnv {
-  readonly NG_APP_WEATHER_API_KEY: string;
   readonly NG_APP_GOOGLE_CLIENT_ID: string;
-  readonly WEATHER_API_KEY: string;
-  readonly GOOGLE_SHEETS_API_KEY: string;
+  readonly NG_APP_WEATHER_API_KEY: string;
+  readonly NG_APP_GOOGLE_SHEETS_API_KEY: string;
   readonly NG_APP_GOOGLE_CLIENT_SECRET: string;
-  readonly GOOGLE_APPS_SCRIPT_URL: string;
-  readonly NEWS_API_TOKEN: string;
+  readonly NG_APP_GOOGLE_APPS_SCRIPT_URL: string;
+  readonly NG_APP_NEWS_API_TOKEN: string;
   // thêm các env khác bạn cần
 }
 
