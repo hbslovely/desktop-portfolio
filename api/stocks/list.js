7fa49cf1cac0004efcc31371e6adfa9f4accc427
Phat Hong <phat.hong@hcl.com>
2025-12-08 11:17:33 +0700

    Update path

--- api/stocks/list.js
+++ api/stocks/list.js
@@ -14,7 +14,7 @@ async function getStockListFromGitHub(
   repoName = 'desktop-portfolio',
   branch = 'master'
 ) {
-  const folderPath = 'src/assets/stocks';
+  const folderPath = 'api/data/stocks';
   const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}?ref=${branch}`;
 
   try {
--- api/stocks/save.js
+++ api/stocks/save.js
@@ -13,7 +13,7 @@ async function commitToGitHub(
   repoName,
   branch = 'master'
 ) {
-  const filePath = `src/assets/stocks/${symbol.toUpperCase()}.json`;
+  const filePath = `api/data/stocks/${symbol.toUpperCase()}.json`;
   const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
 
   try {
