59bbd1315f53d7097991ffb7f32c9a31d2a95401
Phat Hong <phat@backbase.com>
2025-12-19 13:24:59 +0700

    Add lucky wheeel for 2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
     path: 'chat',
     loadComponent: () => import('./pages/chat/chat.component').then(m => m.ChatComponent),
     title: 'Video Chat - WebRTC'
+  },
+  {
+    path: 'hello-2026',
+    loadComponent: () => import('./pages/hello-2026/hello-2026.component').then(m => m.Hello2026Component),
+    title: 'Vòng Quay Tết 2026'
   }
 ];
