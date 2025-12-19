3e2577d1491f8eb0c5dc8a6c9f8ce2a4b3709d56
Phat Hong <phat@backbase.com>
2025-12-19 14:58:38 +0700

    Add PIT calculation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,10 @@ export const routes: Routes = [
     path: 'hello-2026',
     loadComponent: () => import('./pages/hello-2026/hello-2026.component').then(m => m.Hello2026Component),
     title: 'Vòng Quay Tết 2026'
+  },
+  {
+    path: 'pit',
+    loadComponent: () => import('./pages/pit-2026/pit-2026.component').then(m => m.Pit2026Component),
+    title: 'Công cụ tính thuế TNCN 2026'
   }
 ];
