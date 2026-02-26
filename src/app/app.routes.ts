ceaef1661825e8ff7d15b5bd0700fce17faae60b
Phat Hong <phat@backbase.com>
2026-02-26 11:49:36 +0700

    Thêm routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,11 @@ export const routes: Routes = [
     title: 'Vòng Quay Tết 2026'
   },
   {
+    path: 'ten-cua-boi-2026',
+    loadComponent: () => import('./pages/ten-cua-boi-2026/ten-cua-boi-2026.component').then(m => m.TenCuaBoi2026Component),
+    title: 'Tên của Bối'
+  },
+  {
     path: 'pit',
     loadComponent: () => import('./pages/pit-2026/pit-2026.component').then(m => m.Pit2026Component),
     title: 'Công cụ tính thuế TNCN 2026'
